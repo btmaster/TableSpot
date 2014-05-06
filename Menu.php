8a23d6b1beb48a6cbb20dc7be3724388cc2ef069
Bjorn Truyen <bjorn.truyen@gmail.com>
2014-05-06 18:35:09 +0200

    Menu aanmaken

--- Menu.php
+++ Menu.php
@@ -6,8 +6,20 @@
 		include_once("include/rolkeuze.php");
 		include_once("classes/Restaurant.class.php");
 
-		$restaurant = new Restaurant();	
+		$restaurant = new Restaurant();
+		$_GET['id'] = $_SESSION['restaurant'];	
 		$restaurant->SelectedId = $_GET['id'];
+
+		if(!empty($_POST))
+		{
+			include_once("classes/Menu.class.php");
+			$menu = new Menu();
+			$menu->name = $_POST['name'];
+			$menu->description = $_POST['description'];
+			$menu->restaurant = $_SESSION['restaurant'];
+			$menu->Save();
+
+		}
 	} else {
 		header("Location: index.php");
 	}
@@ -25,54 +37,15 @@
  
 include_once("include/navinclude.php");
  
-?><div>
-	<h1>Open Spot</h1>
-		<ul id="sortable_list">
-		<li id="listitem"  class="clearfix header">
-			<div class="listitem_Tafelnummer">Tafelnummer</div>
-			<div class="listitem_Plaatsen">Plaatsen</div>
-			<div class="listitem_Status">Status</div>
-		</li>
-		<?php
-		$result = $restaurant->GetAllfreespots();
-
-	while($freespots = $result->fetch_assoc())
-	    {
-			echo "<li id='listitem_". $freespots["ID_Table"] ."' class='clearfix'>";			
-			echo "<div class='listitem_Tafelnummer'>". $freespots["ID_Table"] ."</div>";
-			echo "<div class='listitem_Plaatsen'>". $freespots["Place"] ."</div>";
-			echo "<div class='listitem_Status'>" . $freespots["Status"] ."</div>";
-			echo "</li>";
-		}
-		?>
-		
-	</ul>
-</div>
-<div>
-	<h1>Reservations</h1>
-		<ul id="sortable_list">
-		<li id="listitem"  class="clearfix header">
-			<div class="listitem_Tafelnummer">Tafelnummer</div>
-			<div class="listitem_Plaatsen">Plaatsen</div>
-			<div class="listitem_Status">Status</div>
-			<div class="listitem_Status">Status</div>
-		</li>
-		<?php
-		$res = $restaurant->GetAllReservations();
-
-	while($Reservation = $res->fetch_assoc())
-	    {
-			echo "<li id='listitem_". $Reservation["ID_Resevation"] ."' class='clearfix'>";			
-			echo "<div class='listitem_Tafelnummer'>". $Reservation["FK_Table_ID"] ."</div>";
-			echo "<div class='listitem_Plaatsen'>". $Reservation["AmountPeople"] ."</div>";
-			echo "<div class='listitem_Status'>" . $Reservation["Date"] ."</div>";
-			echo "<div class='listitem_Status'>" . $Reservation["FK_Customer_ID"] ."</div>";
-			echo "</li>";
-		}
-		?>
-		
-	</ul>
-</div>
+?>
+	<h1>Menu toevoegen</h1>
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+		<label for="name">Naam</label><br/>
+		<input type="text" name="name" id="name"><br/>
+		<label for="description">Beschrijving</label><br/>
+		<input type="text" name="description" id="description"><br/>
+		<input type="submit" name="btnVoegtoe" value="Voeg Toe"><br/>
+	</form>
 		
 	</div>
 	<!-- klant -->
