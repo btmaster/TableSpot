f67d325894e8d739a962e8d1b42817f17b5ec509
hannes <hannesverdoodt@gmail.com>
2014-05-12 03:35:18 +0200

    opmaak nav

--- include/navincludeHouder.php
+++ include/navincludeHouder.php
@@ -1,22 +1,12 @@
 <nav>
-	<li <?php echo (basename($_SERVER['SCRIPT_FILENAME'])=='Home.php'? 'class="active' : '');?>><a href="Home.php">home</a></li>
+	<li <?php echo (basename($_SERVER['SCRIPT_FILENAME'])=='Home.php'? ' class="active' : '');?>><a href="Home.php">home</a></li>
+	<li <?php echo (basename($_SERVER['SCRIPT_FILENAME'])=='Restaurant.php'? ' class="active' : '');?>><a href="Restaurant.php">Restaurant</a></li>
+	<li <?php echo (basename($_SERVER['SCRIPT_FILENAME'])=='Menu.php'? ' class="active' : '');?>><a href="Menu.php?id="<?php echo $_GET['id']; ?>"">Menu</a></li>
+	<li <?php echo (basename($_SERVER['SCRIPT_FILENAME'])=='Tables.php'? ' class="active' : '');?>><a href="Tables.php">Tables</a></li>
 	<?php 	echo "<li" ;
-	        echo (basename($_SERVER['SCRIPT_FILENAME'])=='Restaurant.php'? 'class="active' : '');  
+	        echo (basename($_SERVER['SCRIPT_FILENAME'])=='Restaurant.php'? ' class="active' : '');  
 	        echo "><a href='Restaurant.php?id= " . $_GET['id'] ."'>"; 
-			echo $restaurant->GetTitle() . "</a></li>"; ?>
-	<?php 	echo "<li ";
-			echo (basename($_SERVER['SCRIPT_FILENAME'])=='Menu.php'? 'class="active' : '');  
-			echo "><a href='Menu.php?id= ". $_GET['id']."'>"; 
-			echo "menu</a></li>"; ?>
-	<?php 	echo "<li ";
-			echo (basename($_SERVER['SCRIPT_FILENAME'])=='Tables.php'? 'class="active' : '');  
-			echo "><a href='Tables.php?id= ". $_GET['id']."'>"; 
-			echo "Tables</a></li>"; ?>
-	<?php 	echo "<li ";
-			echo (basename($_SERVER['SCRIPT_FILENAME'])=='Overview.php'? 'class="active' : '');  
-			echo "><a href='Overview.php?id= ". $_GET['id']."'>"; 
-			echo "Overview</a></li>"; ?>
-	<?php 	echo "<li><a href='Logout.php'>Logout</a></li>";?>
+			echo $restaurant->GetTitle() . "</a></li>"; ?>	
 	<li><a href="Logout.php">Log out</a></li>
 
 </nav>
